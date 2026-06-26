acc668dad93dca9036d4568a4c119589de2cfd93
Daanyaal Kellerman <Daanyaal@Daanyaals-MacBook-Air.local>
2026-06-26 08:04:09 +0200

    analytics

--- src/main.js
+++ src/main.js
@@ -1,9 +1,28 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import store from './store'
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import './assets/main.css'
-
 import VueGtag from 'vue-gtag-next'
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD69OJgyj4tkK8_M5XLFyh0Eof6r99CT-E",
+  authDomain: "daanyaal-kellerman.firebaseapp.com",
+  projectId: "daanyaal-kellerman",
+  storageBucket: "daanyaal-kellerman.firebasestorage.app",
+  messagingSenderId: "589793059785",
+  appId: "1:589793059785:web:2c60797c5e59fe846c7e18",
+  measurementId: "G-6KYHJ3FVGC"
+};
+
+
+// Initialize Firebase
+const firebaseapp = initializeApp(firebaseConfig);
+
+// Initialize Analytics
+const analytics = getAnalytics(firebaseapp);
 
 const app = createApp(App)
 
